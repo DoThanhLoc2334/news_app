0e703aeb1cbc159b2d55c60558309f3d961885f1
DoThanhLoc2334 <locdothanh233@gmail.com>
2025-12-11 08:24:55 +0700

    feat: display list+image by using cached_image_network

--- lib/news_screen.dart
+++ lib/news_screen.dart
@@ -2,44 +2,54 @@ import 'package:flutter/material.dart';
 import 'modelArticle.dart';
 import 'api_service.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+
 class NewsScreen extends StatelessWidget {
   const NewsScreen({super.key});
 
-  Widget build(BuildContext context)
-  {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("News"),
-        centerTitle: true,
-      ),
-
+      appBar: AppBar(title: Text("News"), centerTitle: true),
       body: FutureBuilder<List<ModelArticle>>(
         future: ApiService.fetchArticles(),
-        builder: (context, snapshot){
-          if(snapshot.connectionState == ConnectionState.waiting){
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
             return Center(child: CircularProgressIndicator());
           }
 
-          if(snapshot.hasError){
+          if (snapshot.hasError) {
             return Center(child: Text("Error load data"));
           }
 
-          if(!snapshot.hasData || snapshot.data!.isEmpty){
+          if (!snapshot.hasData || snapshot.data!.isEmpty) {
             return Center(child: Text("Don't have news"));
           }
 
           final articles = snapshot.data!;
           return ListView.builder(
             itemCount: articles.length,
-            itemBuilder: (context, index){
+            itemBuilder: (context, index) {
               final article = articles[index];
               return Card(
                 margin: EdgeInsets.all(10),
                 child: ListTile(
                   leading: article.imageUrl.isNotEmpty
-                  ? Image.network(article.imageUrl, width: 80, fit: BoxFit.cover) : Icon(Icons.image_not_supported),
-                  title: Text(article.title),
-                  subtitle: Text(article.description),
+                      ? CachedNetworkImage(
+                    imageUrl: article.imageUrl,
+                    width: 80,
+                    fit: BoxFit.cover,
+                    placeholder: (context, url) =>
+                        Center(child: CircularProgressIndicator()),
+                    errorWidget: (context, url, error) =>
+                        Icon(Icons.broken_image),
+                  )
+                      : Icon(Icons.image_not_supported),
+                  title: Text(article.title, maxLines: 2,),
+                  subtitle: Text(
+                    article.description,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  )
                 ),
               );
             },
