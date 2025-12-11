c8eb9ccec7dd4ea149181d61fb496f1474eb168a
DoThanhLoc2334 <locdothanh233@gmail.com>
2025-12-11 09:28:52 +0700

    feat-pullToRefresh: replace FutureBuilder with RefreshIndicator

--- lib/news_screen.dart
+++ lib/news_screen.dart
@@ -5,7 +5,6 @@ import 'package:cached_network_image/cached_network_image.dart';
 
 class NewsScreen extends StatefulWidget {
   const NewsScreen({super.key});
-
   _NewsScreenState createState() => _NewsScreenState();
 }
 
@@ -13,6 +12,11 @@ class _NewsScreenState extends State<NewsScreen> {
   List<ModelArticle> _articles = [];
   bool _loading = true;
   String? _error;
+
+  void initState() {
+    super.initState();
+    _loadArticles();
+  }
 
   Future<void> _loadArticles() async {
     setState(() {
@@ -25,65 +29,56 @@ class _NewsScreenState extends State<NewsScreen> {
         _articles = data;
         _loading = false;
       });
-    }catch(e){
+    } catch (e) {
+      setState(() {
         _error = e.toString();
         _loading = false;
+      });
     }
   }
 
-  void initState(){
-    super.initState();
-    _loadArticles();
-  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("News"), centerTitle: true),
-      body: FutureBuilder<List<ModelArticle>>(
-        future: ApiService.fetchArticles(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          }
+      body: RefreshIndicator(
+        onRefresh: _loadArticles,
+        child: _loading
+            ? Center(child: CircularProgressIndicator())
+            : _error != null
+            ? Center(child: Text("Error: $_error"))
+            : _articles.isEmpty
+            ? Center(child: Text("Don't have news"))
+            : ListView.builder(
+                physics: AlwaysScrollableScrollPhysics(),
 
-          if (snapshot.hasError) {
-            return Center(child: Text("Error load data"));
-          }
-
-          if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return Center(child: Text("Don't have news"));
-          }
-
-          final articles = snapshot.data!;
-          return ListView.builder(
-            itemCount: articles.length,
-            itemBuilder: (context, index) {
-              final article = articles[index];
-              return Card(
-                margin: EdgeInsets.all(10),
-                child: ListTile(
-                  leading: article.imageUrl.isNotEmpty
-                      ? CachedNetworkImage(
-                    imageUrl: article.imageUrl,
-                    width: 80,
-                    fit: BoxFit.cover,
-                    placeholder: (context, url) =>
-                        Center(child: CircularProgressIndicator()),
-                    errorWidget: (context, url, error) =>
-                        Icon(Icons.broken_image),
-                  )
-                      : Icon(Icons.image_not_supported),
-                  title: Text(article.title, maxLines: 2,),
-                  subtitle: Text(
-                    article.description,
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                  )
-                ),
-              );
-            },
-          );
-        },
+                itemCount: _articles.length,
+                itemBuilder: (context, index) {
+                  final article = _articles[index];
+                  return Card(
+                    margin: EdgeInsets.all(10),
+                    child: ListTile(
+                      leading: article.imageUrl.isNotEmpty
+                          ? CachedNetworkImage(
+                              imageUrl: article.imageUrl,
+                              width: 80,
+                              fit: BoxFit.cover,
+                              placeholder: (context, url) =>
+                                  Center(child: CircularProgressIndicator()),
+                              errorWidget: (context, url, error) =>
+                                  Icon(Icons.broken_image),
+                            )
+                          : Icon(Icons.image_not_supported),
+                      title: Text(article.title, maxLines: 2),
+                      subtitle: Text(
+                        article.description,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  );
+                },
+              ),
       ),
     );
   }
